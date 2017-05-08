7a457f3cb07e207126d9bdbbe4cc860a41d0952f
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-05-07 22:54:15 -0400

    Echo example

---
+++ examples/echo.el
@@ -0,0 +1,19 @@
+
+extern i32 strlen(s str)
+extern str malloc(size i32)
+extern str strcpy(dst str, src str)
+extern i32 puts(s str)
+extern str gets(s str)
+# we can define a limited version of printf that only works for one i32
+extern i32 printf(s str, d i32)
+
+let i32 echo_inner(buf str) =
+    let input = gets(buf) in
+    let _ = puts(buf) in
+    echo_inner(buf)
+
+let i32 echo(maxSize i32) =
+    let buf = malloc(maxSize) in
+    echo_inner(buf)
+
+let i32 main() = echo(100)
